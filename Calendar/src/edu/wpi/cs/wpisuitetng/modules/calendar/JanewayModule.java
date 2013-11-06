079cd78c020b1d0bf9b3be12c06b9e893830382e
ewillcox3 <ewillcox3@gmail.com>
2013-11-05 20:12:19 -0500

    Cal - Attempting to make

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/JanewayModule.java
@@ -0,0 +1,78 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar;
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Chris Casola
+ *    Andrew Hurle
+ ******************************************************************************/
+
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
+import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+
+
+/**
+ * A dummy module to demonstrate the Janeway client
+ *
+ */
+public class JanewayModule implements IJanewayModule {
+	
+	/** The tabs used by this module */
+	private ArrayList<JanewayTabModel> tabs;
+	
+	/**
+	 * Construct a new DummyModule for demonstration purposes
+	 */
+	public JanewayModule() {
+		
+		// Setup button panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(new JButton("Func A"));
+		buttonPanel.add(new JButton("Func B"));
+		
+		// Setup the main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(new JLabel("Calendar"), BorderLayout.PAGE_START);
+		mainPanel.add(new JTextField(), BorderLayout.CENTER);
+		mainPanel.add(new JTextField(), BorderLayout.CENTER);
+		
+		tabs = new ArrayList<JanewayTabModel>();
+		JanewayTabModel tab = new JanewayTabModel("Calendar", new ImageIcon(), buttonPanel, mainPanel);
+		tabs.add(tab);
+	}
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
+	 */
+	@Override
+	public String getName() {
+		return "Calendar";
+	}
+
+	/**
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
+	 */
+	@Override
+	public List<JanewayTabModel> getTabs() {
+		return tabs;
+	}
+}
