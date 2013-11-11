f9ea882de0f03fbb06c814826bc64c6c12080ae4
ewillcox3 <ewillcox3@gmail.com>
2013-11-11 16:27:59 -0500

    Updates to a file while testing Checkstyle.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/Calendar.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/Calendar.java
@@ -7,67 +7,69 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Yuchen Zhang
+ * Team 3
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.calendar;
 
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
-import javax.swing.JPanel;
 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.MainView;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.ToolbarView;
 
-
 /**
- * A Calendar Module for WPI-Suite
- *
+ * A Calendar Module for WPI-Suite.
+ * 
  */
 public class Calendar implements IJanewayModule {
-	
-	/** The tabs used by this module */
-	private List<JanewayTabModel> tabs;
-	
-	/**
-	 * Construct a new Calendar Module for demonstration purposes
-	 */
-	public Calendar() {
-		
-		// Setup button panel
-		ToolbarView toolbar = new ToolbarView();
-//		buttonPanel.setLayout(new FlowLayout());
-//		buttonPanel.add(new JButton("Func A"));
-//		buttonPanel.add(new JButton("Func B"));
-		
-		// Setup the main panel
-		MainView mainPanel = new MainView();
-		mainPanel.setLayout(new GridLayout(1, 2));
-		
-		tabs = new ArrayList<JanewayTabModel>();
-		JanewayTabModel tab = new JanewayTabModel("Calendar", new ImageIcon(), toolbar, mainPanel);
-		tabs.add(tab);
-	}
 
-	/**
-	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
-	 */
-	@Override
-	public String getName() {
-		return "Calendar";
-	}
+  /** The tabs used by this module. */
+  private List<JanewayTabModel> tabs;
 
-	/**
-	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
-	 */
-	@Override
-	public List<JanewayTabModel> getTabs() {
-		return tabs;
-	}
+  /**
+   * Construct a new Calendar Module for demonstration purposes.
+   */
+  public Calendar() {
+
+    // Setup button panel
+    final ToolbarView toolbar = new ToolbarView();
+    // buttonPanel.setLayout(new FlowLayout());
+    // buttonPanel.add(new JButton("Func A"));
+    // buttonPanel.add(new JButton("Func B"));
+
+    // Setup the main panel
+    final MainView mainPanel = new MainView();
+    mainPanel.setLayout(new GridLayout(1, 2));
+
+    tabs = new ArrayList<JanewayTabModel>();
+    final JanewayTabModel tab = new JanewayTabModel("Calendar", new ImageIcon(),
+        toolbar, mainPanel);
+    tabs.add(tab);
+  }
+
+  /**
+   * Getter for name.
+   * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
+   * @return A string saying it is a calendar.
+   */
+  @Override
+  public String getName () {
+    return "Calendar";
+  }
+
+  /**
+   * Getter for tabs.
+   * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
+   * @return A list of the tabs.
+   */
+  @Override
+  public List<JanewayTabModel> getTabs() {
+    return tabs;
+  }
 }
