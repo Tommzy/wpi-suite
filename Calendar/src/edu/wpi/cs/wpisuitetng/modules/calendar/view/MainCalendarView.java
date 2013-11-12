bc8be2efa7479586cd2523b3313bdb19ea1acef4
ewillcox3 <ewillcox3@gmail.com>
2013-11-11 19:47:48 -0500

    All GUI done with CS

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MainCalendarView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/MainCalendarView.java
@@ -12,70 +12,82 @@
 
 package edu.wpi.cs.wpisuitetng.modules.calendar.view;
 
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 
 import edu.wpi.cs.wpisuitetng.modules.calendar.controller.MainCalendarController;
 import edu.wpi.cs.wpisuitetng.modules.calendar.master.CalendarTimePeriod;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.MainCalendarModel;
 
+// TODO: Auto-generated Javadoc
 /**
- * This Panel sets the calendar area within main view of Calendar module.
- * It contains a sub panel CalendarView and 
- * buttons to switch among day-view, week-view, month-view and year-view. 
+ * This Panel sets the calendar area within main view of Calendar module. It
+ * contains a sub panel CalendarView and buttons to switch among day-view,
+ * week-view, month-view and year-view.
  * 
  * @author Yuchen Zhang
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class MainCalendarView extends JPanel {
-	List<JToggleButton> tglbtnPeriod = new ArrayList<JToggleButton>();
-	JPanel calendarView = new JPanel();
+  
+  /** The tglbtn period. */
+  List<JToggleButton> tglbtnPeriod = new ArrayList<JToggleButton>();
+  
+  /** The calendar view. */
+  JPanel              calendarView = new JPanel();
 
-	
-	/**
-	 * Create the calendar area within main panel.
-	 * 
-	 * @param mainCalendarModel
-	 */
-	public MainCalendarView(MainCalendarModel mainCalendarModel) {
-		// Create toggle buttons for different calendar view
-		JPanel calendarViewSwitch = new JPanel();
-		calendarViewSwitch.setLayout(new BoxLayout(calendarViewSwitch, BoxLayout.X_AXIS));
-		for (CalendarTimePeriod value : CalendarTimePeriod.values()) {
-			JToggleButton tglbtn = new JToggleButton(value.name());
-			tglbtnPeriod.add(tglbtn);
-			tglbtn.addActionListener(new MainCalendarController(mainCalendarModel, this));
-			calendarViewSwitch.add(tglbtn);
-		}
-		add(calendarViewSwitch);
-		
-		add(calendarView);
-		
-		initialize();
-	}
-	
-	/**
-	 * Getter for tglbtnPeriod
-	 * @return tglbtnPeriod
-	 */
-	public List<JToggleButton> getToggleButtonPeriod() {
-		return tglbtnPeriod;
-	}
-	
-	public JPanel getCalendarView() {
-		return calendarView;
-	}
-	
-	private void initialize() {
-		tglbtnPeriod.get(3).doClick();
-	}
+  /**
+   * Create the calendar area within main panel.
+   *
+   * @param mainCalendarModel the main calendar model
+   */
+  public MainCalendarView(MainCalendarModel mainCalendarModel) {
+    // Create toggle buttons for different calendar view
+    final JPanel calendarViewSwitch = new JPanel();
+    calendarViewSwitch.setLayout(new BoxLayout(calendarViewSwitch,
+        BoxLayout.X_AXIS));
+    for (CalendarTimePeriod value : CalendarTimePeriod.values()) {
+      final JToggleButton tglbtn = new JToggleButton(value.name());
+      tglbtnPeriod.add(tglbtn);
+      tglbtn.addActionListener(new MainCalendarController(mainCalendarModel,
+          this));
+      calendarViewSwitch.add(tglbtn);
+    }
+    add(calendarViewSwitch);
+
+    add(calendarView);
+
+    initialize();
+  }
+
+  /**
+   * Getter for tglbtnPeriod.
+   *
+   * @return tglbtnPeriod
+   */
+  public final List<JToggleButton> getToggleButtonPeriod() {
+    return tglbtnPeriod;
+  }
+
+  /**
+   * Gets the calendar view.
+   *
+   * @return the calendar view
+   */
+  public final JPanel getCalendarView() {
+    return calendarView;
+  }
+
+  /**
+   * Initialize.
+   */
+  private void initialize() {
+    tglbtnPeriod.get(3).doClick();
+  }
 
 }
--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/TaskTable.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/TaskTable.java
@@ -15,76 +15,121 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import net.miginfocom.swing.MigLayout;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class TaskTable.
+ */
+@SuppressWarnings("serial")
 public class TaskTable extends JPanel {
 
+  /**
+   * Instantiates a new task table.
+   */
   public TaskTable() {
     super(new GridLayout(1, 0));
 
-    JTable table = new JTable(new TaskTableModel());
+    final JTable table = new JTable(new TaskTableModel());
     table.setPreferredScrollableViewportSize(new Dimension(500, 80));
     table.setFillsViewportHeight(true);
     table.setAutoCreateRowSorter(true);
 
     // Create the scroll pane and add the table to it.
-    JScrollPane scrollPane = new JScrollPane(table);
+    final JScrollPane scrollPane = new JScrollPane(table);
 
     // Add the scroll pane to this panel.
     add(scrollPane);
   }
 
+  /**
+   * The Class TaskTableModel.
+   */
+  @SuppressWarnings("serial")
   class TaskTableModel extends AbstractTableModel {
-    private String[]   columnNames = {
-                                       "Task", "Date", "Time"
-                                   };
-    private Object[][] data        = {
-                                       {
-                                           "GUI HW", "Nov 13, 2013", "09:00"
-                                       },
-                                       {
-                                           "DB HW", "Nov 13, 2013", "10:00"
-                                       },
-                                       {
-                                           "Pick a layout", "Nov 13, 2013",
-                                           "12:00"
-                                       },
-                                       {
-                                           "Decide on a DB structure",
-                                           "Nov 13, 2013", "14:30"
-                                       },
-                                       {
-                                           "Call PM", "Nov 13, 2013",
-                                           "16:50"
-                                       },
-                                       {
-                                           "Write blog", "Nov 13, 2013",
-                                           "16:55"
-                                       }
-                                   };
+    
+    /** The column names. */
+    private String[]   columnNames = 
+    {
+      "Task", "Date", "Time",
+    };
+    
+    /** The data. */
+    private Object[][] data        = 
+    {
+      {
+        "GUI HW", "Nov 13, 2013", "09:00",
+      },
+      {
+        "DB HW", "Nov 13, 2013", "10:00",
+      },
+      {
+        "Pick a layout", "Nov 13, 2013",
+        "12:00",
+      },
+      {
+        "Decide on a DB structure",
+        "Nov 13, 2013", "14:30",
+      },
+      {
+        "Call PM", "Nov 13, 2013",
+        "16:50",
+      },
+      {
+        "Write blog", "Nov 13, 2013",
+        "16:55",
+      },
+    };
 
     
+    /**
+     * Gives the number of columns in the table.
+     * @see javax.swing.table.TableModel#getColumnCount()
+     * @return The number of columns in the table.
+     */
     public int getColumnCount() {
       return columnNames.length;
     }
 
+    /**
+     * Gives the number of rows total in the table.
+     * @see javax.swing.table.TableModel#getRowCount()
+     * @return The number of rows in the table.
+     */
     public int getRowCount() {
       return data.length;
     }
 
+    /**
+     * Gets the name of the column as a string.
+     * @see javax.swing.table.AbstractTableModel#getColumnName(int)
+     * @param col The column name to check.
+     * @return A string that names the column.
+     */
     public String getColumnName(int col) {
       return columnNames[col];
     }
 
+    /**
+     * Get the value of a cell at a row and column.
+     * @see javax.swing.table.TableModel#getValueAt(int, int)
+     * @param row The row to check.
+     * @param col The column to check.
+     * @return The data located at row, col.
+     */
     public Object getValueAt(int row, int col) {
       return data[row][col];
     }
 
-    /*
+    /**
      * JTable uses this method to determine the default renderer/editor for each
      * cell. If we didn't implement this method, then the last column would
      * contain text ("true"/"false"), rather than a check box.
+     *
+     * @see javax.swing.table.AbstractTableModel#getColumnClass(int)
+     * @param col The column to check
+     * @return The class of the data at 0, col.
      */
-    public Class getColumnClass(int c) {
-      return getValueAt(0, c).getClass();
+    public Class getColumnClass(int col) {
+      return getValueAt(0, col).getClass();
     }
   }
 
@@ -94,18 +139,18 @@ public class TaskTable extends JPanel {
    */
   private static void createAndShowGUI() {
     // Create and set up the window.
-    JFrame frame = new JFrame("Task Table");
+    final JFrame frame = new JFrame("Task Table");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     // Using Mig for JPanels to help with layout.
-    JPanel panel = new JPanel(new MigLayout());
+    final JPanel panel = new JPanel(new MigLayout());
 
     // Table label
-    JLabel eventTableLabel = new JLabel("Tasks");
+    final JLabel eventTableLabel = new JLabel("Tasks");
     eventTableLabel.setFont(new Font("Arial", Font.BOLD, 16));
 
     // Create and set up the content pane for the table.
-    TaskTable newContentPane = new TaskTable();
+    final TaskTable newContentPane = new TaskTable();
     newContentPane.setOpaque(true); // content panes must be opaque
     // Add in the title, make the next add go down to the next row
     panel.add(eventTableLabel, "align center");
@@ -119,6 +164,11 @@ public class TaskTable extends JPanel {
     frame.setVisible(true);
   }
 
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
   public static void main(String[] args) {
     // Schedule a job for the event-dispatching thread:
     // creating and showing this application's GUI.
