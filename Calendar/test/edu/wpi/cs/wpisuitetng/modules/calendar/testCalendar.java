ff0aa5a54df8eefa6620d2aaa8486d00ea302a44
Yuchen Zhang <yuchenzhang@wpi.edu>
2013-11-16 18:43:01 -0500

    Modified CalendarDayView and Made DayEvent object for that view. Fixed "week" not displaying issue. Created testCalendar class to store tests on GUI.

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/testCalendar.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/testCalendar.java
@@ -12,6 +12,34 @@
 
 package edu.wpi.cs.wpisuitetng.modules.calendar;
 
-public class testCalendar {
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
+import net.miginfocom.swing.MigLayout;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.MainView;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.ToolbarView;
+
+public class testCalendar {
+	
+	private static void testGUI() {
+		JFrame cal = new JFrame();
+		cal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		JPanel calPane =  new JPanel(new MigLayout());
+		ToolbarView toolbar = new ToolbarView();
+		MainView calendar = new MainView();
+		calendar.setSize(cal.getMaximumSize());
+		calPane.add(toolbar, "wrap");
+		calPane.add(calendar);
+		
+		cal.add(calPane);
+		
+		cal.pack();
+		cal.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		testGUI();
+	}
 }
