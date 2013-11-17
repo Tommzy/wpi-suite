453aa48e31c7baae65cdd680c6379e733dfa1f1b
Yuchen Zhang <yuchenzhang@wpi.edu>
2013-11-17 13:13:22 -0500

    Modified CalendarDayView so that it can handle overlapping events; made a mock up for day view in janeway.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/MainCalendarController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/MainCalendarController.java
@@ -7,20 +7,25 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Yuchen Zhang
+ *    Team3
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.calendar.controller;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.GregorianCalendar;
 
 import javax.swing.JToggleButton;
 
 import edu.wpi.cs.wpisuitetng.modules.calendar.master.CalendarTimePeriod;
+import edu.wpi.cs.wpisuitetng.modules.calendar.master.DayEvent;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.MainCalendarModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarMonthView;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarWeekView;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarYearView;
+import edu.wpi.cs.wpisuitetng.modules.calendar.view.CalendarDayView;
 import edu.wpi.cs.wpisuitetng.modules.calendar.view.MainCalendarView;
 
 /**
@@ -63,8 +68,17 @@ public class MainCalendarController implements ActionListener{
 				view.getCalendarView().add(monthView);
 				break;
 			case "Week" :
+				CalendarWeekView weekView = new CalendarWeekView();
+				view.getCalendarView().add(weekView);
 				break;
 			case "Day" :
+				DayEvent[] sampleEvent = {new DayEvent("Whoops", new GregorianCalendar(2013, 5, 21, 20, 50, 0), new GregorianCalendar(2013, 5, 21, 21, 5, 0)), 
+				new DayEvent("Innebandy", new GregorianCalendar(2013, 5, 21, 15, 50, 0), new GregorianCalendar(2013, 5, 21, 16, 5, 0)), 
+				new DayEvent("Abcd", new GregorianCalendar(2013, 5, 21, 15, 55, 0), new GregorianCalendar(2013, 5, 21, 16, 15, 0)), 
+				new DayEvent("Efgh", new GregorianCalendar(2013, 5, 21, 15, 55, 0), new GregorianCalendar(2013, 5, 21, 16, 15, 0)),
+				new DayEvent("Hey", new GregorianCalendar(2013, 5, 21, 8, 50, 0), new GregorianCalendar(2013, 5, 21, 10, 5, 0)) };
+				CalendarDayView dayView = new CalendarDayView(sampleEvent);
+				view.getCalendarView().add(dayView);
 				break;
 			default: break;
 		}
