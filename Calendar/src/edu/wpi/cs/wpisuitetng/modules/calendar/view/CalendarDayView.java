17f92c8d9270501d2be8e77218d5e8fe3cd0b085
Yuchen Zhang <yuchenzhang@wpi.edu>
2013-11-17 13:13:22 -0500

    Modified CalendarDayView so that it can handle overlapping events; made a mock up for day view in janeway.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarDayView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarDayView.java
@@ -10,7 +10,6 @@
  *    Team3
  ******************************************************************************/
 
-
 package edu.wpi.cs.wpisuitetng.modules.calendar.view;
 
 import java.awt.BorderLayout;
@@ -18,10 +17,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -43,23 +46,33 @@ public class CalendarDayView extends JPanel {
 	JScrollPane scroll = new JScrollPane(view,
 			JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 	ArrayList<EventCard> eventCards = new ArrayList<EventCard>();
+	HashMap<EventCard, GridBagConstraints> eventConstraint = new HashMap<EventCard, GridBagConstraints>();
 	final int minimalInterval = 2;
+	int currentMaxWidth = 1;
+	Calendar date = GregorianCalendar.getInstance();
+	String[] weekdays = new DateFormatSymbols().getWeekdays();
 
 	public CalendarDayView() {
 		GridBagConstraints c = new GridBagConstraints();
 		setLayout(new BorderLayout());
 		scroll.setMinimumSize(new Dimension(100, 500));
 		initTimeLabels();
-
+		JLabel header = new JLabel(format(date.get(GregorianCalendar.MONTH) + 1) + "-" + format(date.get(GregorianCalendar.DATE)) + "-" + date.get(GregorianCalendar.YEAR) + " " + weekdays[date.get(GregorianCalendar.DAY_OF_WEEK)]);
+		header.setHorizontalAlignment(SwingConstants.CENTER);
+		add(header, BorderLayout.NORTH);
 		add(scroll, BorderLayout.CENTER);
 	}
-
+	
 	public CalendarDayView(ArrayList<DayEvent> events) {
 		GridBagConstraints c = new GridBagConstraints();
 		setLayout(new BorderLayout());
-		scroll.setMinimumSize(new Dimension(100, 500));
+		//scroll.setMinimumSize(new Dimension(100, 500));
 		initTimeLabels();
-
+		
+		JLabel header = new JLabel(format(date.get(GregorianCalendar.MONTH) + 1) + "-" + format(date.get(GregorianCalendar.DATE)) + "-" + date.get(GregorianCalendar.YEAR) + " " + weekdays[date.get(GregorianCalendar.DAY_OF_WEEK)]);
+		header.setHorizontalAlignment(SwingConstants.CENTER);
+		add(header, BorderLayout.NORTH);
+		
 		for (int i=0; i < events.size(); i++) {
 			addEvent(events.get(i));
 		}
@@ -70,10 +83,13 @@ public class CalendarDayView extends JPanel {
 	public CalendarDayView(DayEvent[] events) {
 		GridBagConstraints c = new GridBagConstraints();
 		setLayout(new BorderLayout());
-		scroll.setMinimumSize(new Dimension(100, 500));
+		//scroll.setMinimumSize(new Dimension(100, 500));
 		initTimeLabels();
 
-		System.out.println("length: " + events.length);
+		JLabel header = new JLabel(format(date.get(GregorianCalendar.MONTH) + 1) + "-" + format(date.get(GregorianCalendar.DATE)) + "-" + date.get(GregorianCalendar.YEAR) + " " + weekdays[date.get(GregorianCalendar.DAY_OF_WEEK)]);
+		header.setHorizontalAlignment(SwingConstants.CENTER);
+		add(header, BorderLayout.NORTH);
+		
 		for (int i=0; i < events.length; i++) {
 			addEvent(events[i]);
 		}
@@ -98,13 +114,13 @@ public class CalendarDayView extends JPanel {
 			//Even hours
 			c.gridy = i;
 			if (i % (60/minimalInterval) == 0) {
-				JLabel timeLabel = new JLabel((i / (60/minimalInterval) <= 9 ? "0" + i / (60/minimalInterval) : i / (60/minimalInterval)) + ":00  ");
+				JLabel timeLabel = new JLabel(format(i / (60/minimalInterval)) + ":00  ");
 				timeLabel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.lightGray));
 				view.add(timeLabel, c);
 			} 
-			//Half hours
+			//Minutes
 			else {
-				//Add an empty box, making the half hours take place as well
+				//Add an empty box, making the minutes take place as well
 				Box box = Box.createVerticalBox();
 				box.add(Box.createVerticalStrut(1));
 				box.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.lightGray));
@@ -114,41 +130,61 @@ public class CalendarDayView extends JPanel {
 	}
 
 	public void addEvent(DayEvent event) {
-		//Events overlapping with the new event is put into an array, and will
-		//later be removed
-		ArrayList<EventCard> toRemove = new ArrayList<EventCard>();
+		ArrayList<EventCard> conflict = new ArrayList<EventCard>();
+		int newGridX = 0;
+		boolean hasOverlap = false;
 		//
-		final JLabel newEvent = new JLabel(event.getEventName()); 
-		// final JLabel newEvent = new JLabel(event.getEndTime().toString());
+		String label = "";
+		if (event.getTimeSpan() < 30) {
+			label = "<HTML><p>" + event.getEventName() + "</p></HTML>";
+		}
+		if (event.getTimeSpan() >= 30 ) {
+			label = "<HTML><p style='text-align:center'>" + event.getEventName() + "<br />" + 
+					format(event.getStartTime().get(GregorianCalendar.HOUR_OF_DAY)) + ":" +
+					format(event.getStartTime().get(GregorianCalendar.MINUTE)) + " - " +
+					format(event.getEndTime().get(GregorianCalendar.HOUR_OF_DAY)) + ":" +
+					format(event.getEndTime().get(GregorianCalendar.MINUTE)) + "</p></HTML>";
+		}
+		final JLabel newEvent = new JLabel(); 
+		newEvent.setText(label);
 		newEvent.setVerticalAlignment(SwingConstants.TOP);
 		newEvent.setHorizontalAlignment(SwingConstants.CENTER);
 		newEvent.setOpaque(true);   //Make the label show it's background
 		newEvent.setBackground(new Color(200, 240, 200));
-		newEvent.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 0, Color.lightGray));
+		newEvent.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.lightGray));
+		newEvent.setPreferredSize(new Dimension (100, newEvent.getMinimumSize().height));
 
-		int labelSize = event.getTimeSpan() / minimalInterval; // 1 min = 1/2 px height
+		int labelSize = event.getTimeSpan() / minimalInterval; // 1 min = 1/minimalInterval px height
 
 		for (EventCard eventCard : eventCards) {
 			if (event.isActiveDuringTimeStamp(eventCard.event.getStartTime())
 					|| event.isActiveDuringTimeStamp(eventCard.event.getEndTime())) {
-				toRemove.add(eventCard); //Which events conflict with the new one
-
+				conflict.add(eventCard); //Which events conflict with the new one
+				newGridX = newGridX < eventConstraint.get(eventCard).gridx? eventConstraint.get(eventCard).gridx : newGridX;
+				currentMaxWidth = currentMaxWidth < newGridX + 1? newGridX + 1: currentMaxWidth;
+				hasOverlap = true;
 			}
 		}
-//		for (final EventCard eventCard : toRemove) {
-//			eventCards.remove(eventCard);
-//			SwingUtilities.invokeLater(new Runnable() {
-//				//Remove the conflicting events
-//				public void run() {
-//					view.remove(eventCard.eventLabel);
-//				}
-//			});
+		
+		// Resize event width
+//		for (EventCard eventCard : eventCards) {
+//			if (hasOverlap && (!conflict.contains(eventCard)) ) {
+//				eventConstraint.get(eventCard).gridwidth++;
+//				view.remove(eventCard.eventLabel);
+//				view.add(eventCard.eventLabel, eventConstraint.get(eventCard));
+//			}
 //		}
+		
 		final GridBagConstraints cLocal = new GridBagConstraints();
 		//Determine where to put the new event
-		cLocal.gridy = (60/minimalInterval) * event.getStartTime().get(event.getStartTime().HOUR_OF_DAY) + event.getStartTime().get(event.getStartTime().MINUTE) / minimalInterval;
-		cLocal.gridheight = labelSize;  cLocal.fill = GridBagConstraints.BOTH;
-		cLocal.gridx = 2; cLocal.weightx = 1; cLocal.weighty = 1;
+		cLocal.gridy = (60/minimalInterval) * event.getStartTime().get(GregorianCalendar.HOUR_OF_DAY) + event.getStartTime().get(GregorianCalendar.MINUTE) / minimalInterval;
+		cLocal.gridheight = labelSize;  
+		cLocal.fill = GridBagConstraints.BOTH;
+		cLocal.gridx = newGridX + 1;
+		//cLocal.gridwidth = currentMaxWidth - newGridX;
+		cLocal.gridwidth = 1;
+		cLocal.weightx = 1; 
+		cLocal.weighty = 1;
 		SwingUtilities.invokeLater(new Runnable() {
 			//Just add the new event at the correct position
 			public void run() {
@@ -156,7 +192,9 @@ public class CalendarDayView extends JPanel {
 			}
 		});
 		//Keep track of the new event, and revalidate/repaint the view
-		eventCards.add(new EventCard(event, newEvent));
+		EventCard eventCard = new EventCard(event, newEvent);
+		eventCards.add(eventCard);
+		eventConstraint.put(eventCard, cLocal);
 		revalidate();
 		repaint();
 	}
@@ -175,12 +213,34 @@ public class CalendarDayView extends JPanel {
 			this.eventLabel = eventLabel;
 		}
 	}
+	
+	private String format(int i) {
+		if (i <= 9) {
+			return "0" + i;
+		}
+		else
+			return Integer.toString(i);
+	}
+	
+	private enum weekday {
+		Sunday,
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday
+	}
 
 	//Test the detailed view, adding some new events
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		CalendarDayView d = new CalendarDayView();
-		d.addEvent(new DayEvent("Innebandy", new GregorianCalendar(2013, 5, 21, 15, 50, 0), new GregorianCalendar(2013, 5, 21, 16, 05, 0))); 
+		d.addEvent(new DayEvent("Whoops", new GregorianCalendar(2013, 5, 21, 20, 50, 0), new GregorianCalendar(2013, 5, 21, 22, 5, 0))); 
+		d.addEvent(new DayEvent("Innebandy", new GregorianCalendar(2013, 5, 21, 15, 50, 0), new GregorianCalendar(2013, 5, 21, 16, 5, 0))); 
+		d.addEvent(new DayEvent("Abcd", new GregorianCalendar(2013, 5, 21, 15, 55, 0), new GregorianCalendar(2013, 5, 21, 16, 15, 0))); 
+		d.addEvent(new DayEvent("Efgh", new GregorianCalendar(2013, 5, 21, 15, 55, 0), new GregorianCalendar(2013, 5, 21, 16, 15, 0))); 
+		d.addEvent(new DayEvent("Hey", new GregorianCalendar(2013, 5, 21, 8, 50, 0), new GregorianCalendar(2013, 5, 21, 9, 21, 0))); 
 		
 		JScrollPane scroll = new JScrollPane(d);
 		
