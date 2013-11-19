83097d840fdf578a5ac52177f266c19824900c5c
Hui Zheng <hzheng@wpi.edu>
2013-11-19 11:22:54 -0500

    Clean codes
    Signed-off-by: Hui Zheng <fengyimzy@gmail.com>

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/ManagerLayer.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/ManagerLayer.java
@@ -35,7 +35,7 @@ import edu.wpi.cs.wpisuitetng.modules.defecttracker.entitymanagers.DefectManager
 import edu.wpi.cs.wpisuitetng.modules.postboard.model.PostBoardEntityManager;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementEntityManager;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.iterations.IterationEntityManager;
-import edu.wpi.cs.wpisuitetng.modules.calendar.model.CommitmentEntityManager;//CommitmentEntityManager;
+
 
 /**
  * This singleton class responds to API requests directed at 
@@ -76,8 +76,8 @@ public class ManagerLayer {
 		map.put("postboardpostboardmessage", new PostBoardEntityManager(data));
 		map.put("requirementmanager" + "requirement", new RequirementEntityManager(data));
 		map.put("requirementmanager" + "iteration", new IterationEntityManager(data));
-		map.put("commitment"+"commitmentEntityManager", new CommitmentEntityManager(data));
-		map.put("commitment"+"commitmentEntityManager", new EventEntityManager(data));
+//		map.put("commitment"+"commitmentEntityManager", new CommitmentEntityManager(data));
+//		map.put("commitment"+"commitmentEntityManager", new EventEntityManager(data));
 
 		//add just your module to this list
 		String[] fullModuleList = {"core","defecttracker","postboard","requirementmanager"};
