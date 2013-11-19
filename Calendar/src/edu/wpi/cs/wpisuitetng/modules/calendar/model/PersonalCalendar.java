33a4c5b2a8c91d98d259d41050c7a943ae00f77f
Brittany Owens <baowens@wpi.edu>
2013-11-18 20:36:59 -0500

    Added CalendarStructure classes
    Data structures to allow us to store all events, commitments, etc.
    associated with a calendar in one place. Also added a PersonalCalendar
    class which has info about the user it belongs to, and a TeamCalendar
    class which has info about the project it belongs to.

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/PersonalCalendar.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Brittany Owens
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.calendar.model;
+
+/**
+ * This is a class for a Personal Calendar structure. It includes information about the user it belongs to.
+ * 
+ * @author Team3
+ * 
+ */
+public class PersonalCalendar extends CalendarStructure{
+	//These data fields may become redundant, but for now they are included for convenience.
+  String userName;
+  int userID;
+}
---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/TeamCalendar.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Brittany Owens
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.calendar.model;
+
+/**
+ * This is a class for a Team Calendar structure. It includes information about the project it belongs to.
+ * 
+ * @author Team3
+ * 
+ */
+
+public class TeamCalendar extends CalendarStructure{
+  //These data fields may become redundant, but for now they are included for convenience.
+  String projName;
+  int projID;
+}
