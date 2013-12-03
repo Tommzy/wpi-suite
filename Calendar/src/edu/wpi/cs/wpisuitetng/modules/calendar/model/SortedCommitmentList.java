1b594cdd32fea22741e7b6791ac3aaee29b4509a
Brittany Owens <baowens@wpi.edu>
2013-12-02 20:58:52 -0500

    -Changed SortedEventList and SortedCommitmentList to separate classes with no relation rather than children of SortedCalendarItemList. Will delete SortedCalendarItemList soon.

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/SortedCommitmentList.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/SortedCommitmentList.java
@@ -13,36 +13,64 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 
 import java.util.GregorianCalendar;
 import java.util.ArrayList;
+import javax.swing.AbstractListModel;
 
-public class SortedCommitmentList extends SortedCalendarItemList{
+public class SortedCommitmentList extends AbstractListModel<Commitment>{
+
+  private ArrayList<Commitment> itemList;
   
-	
   /**
-   * This constructor is for use by other parts of the code.
+   * This is the general-use constructor.
    */
-  SortedCommitmentList() {
-    
+  public SortedCommitmentList() {
+    itemList = new ArrayList<Commitment>();
   }
+  
   /**
    * This constructor is to be used by this class only!
    * @param itemList list of events to be used. Should already be sorted.
    */
-  private SortedCommitmentList(ArrayList<CalendarItem> itemList) {
-    super(itemList);
+  private SortedCommitmentList(ArrayList<Commitment> newitemList) {
+    this.itemList = newitemList;
   }
+  
+  /**
+   * Add the given Commitment to the given index.
+   * @param newItem The commitment to be added to the list
+   * @param where The index to add the new item at.
+   */
+  private void addItemHere(Commitment newItem, int where) {
+    itemList.ensureCapacity(itemList.size() + 1);
+    if (where >= itemList.size()) {
+      itemList.add(newItem);
+    }
+    else {
+        itemList.add(where, newItem);
+    }//end else
+  }//end function
+  
   
   /**Add a new commitment to the list.
    * @param newCommitment the commitment to be added.
    */
   public void addCommitment(Commitment newCommitment) {
-    addCalendarItem((CalendarItem)newCommitment);
+    for (int i = 0; i < itemList.size(); i++) {
+      if (newCommitment.getStartTime().before(this.getElementAt(i).getStartTime())) {
+        addItemHere(newCommitment, i);
+        return;
+      }//end if
+    
+    }//end for
+    //If we get here, our new item was not before anything in the list.
+    //Therefore, it should go to the end of the list.
+    addItemHere(newCommitment, itemList.size());
   }
 
   /**Remove the given commitment from the list.
    * @param whatToRemove commitment to remove from the list.
    */
   public void removeCommitment(Commitment whatToRemove) {
-    removeCalendarItem((CalendarItem) whatToRemove);
+    itemList.remove(whatToRemove);
   }
   
   /**
@@ -53,9 +81,31 @@ public class SortedCommitmentList extends SortedCalendarItemList{
    * @return SortedCommitmentList of the dates in the range.
    */
   public SortedCommitmentList getCommitmentsBetween(GregorianCalendar startRange, GregorianCalendar endRange) {
-	  ArrayList<CalendarItem> newItemList;
-	  newItemList = getDateRange(startRange, endRange);
-	  return new SortedCommitmentList(newItemList);
+    ArrayList<Commitment> itemsInRange = new ArrayList<Commitment>(itemList.size());
+    for (int i = 0; i < itemList.size(); i++) {
+      if ( ( (this.getElementAt(i).getStartTime().after(startRange)) && 
+          (this.getElementAt(i).getStartTime().before(endRange)) )
+          || (this.getElementAt(i).getStartTime().equals(startRange)) || 
+              (this.getElementAt(i).getStartTime().equals(endRange)) ) {
+        itemsInRange.add(this.getElementAt(i));
+      }//end if
+    }//end for
+	  return new SortedCommitmentList(itemsInRange);
+  }
+  
+  
+  
+  /**
+   * @return the number of items in the list
+   */
+  public int getSize() {
+    return itemList.size();
+  }
+
+  @Override
+  public Commitment getElementAt(int index) {
+    Object[] itemArray = itemList.toArray();
+    return (Commitment) itemArray[index];
   }
   
 }
--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/SortedEventList.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/SortedEventList.java
@@ -14,34 +14,61 @@ package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 import java.util.GregorianCalendar;
 import java.util.ArrayList;
 
-public class SortedEventList extends SortedCalendarItemList{
+import javax.swing.AbstractListModel;
+
+public class SortedEventList extends AbstractListModel<Event>{
+  private ArrayList<Event> itemList;
 	
   /**
    * This constructor is for use by other parts of the code.
    */
   SortedEventList() {
-    
+    itemList = new ArrayList<Event>();
   }
   /**
    * This constructor is to be used by this class only!
-   * @param itemList list of events to be used. Should already be sorted.
+   * @param newItemList list of events to be used. Should already be sorted.
    */
-  private SortedEventList(ArrayList<CalendarItem> itemList) {
-    super(itemList);
+  private SortedEventList(ArrayList<Event> newItemList) {
+    this.itemList = newItemList;
   }
+  
+  /**
+   * Add the given Event to the given index.
+   * @param newItem The Event to be added to the list
+   * @param where The index to add the new item at.
+   */
+  private void addItemHere(Event newItem, int where) {
+    itemList.ensureCapacity(itemList.size() + 1);
+    if (where >= itemList.size()) {
+      itemList.add(newItem);
+    }
+    else {
+        itemList.add(where, newItem);
+    }//end else
+  }//end function
 
   /**Add a new event to the list.
    * @param newEvent the event to be added.
    */
   public void addEvent(Event newEvent) {
-    addCalendarItem((CalendarItem)newEvent);
+    for (int i = 0; i < itemList.size(); i++) {
+      if (newEvent.getStartTime().before(this.getElementAt(i).getStartTime())) {
+        addItemHere(newEvent, i);
+        return;
+      }//end if
+    
+    }//end for
+    //If we get here, our new item was not before anything in the list.
+    //Therefore, it should go to the end of the list.
+    addItemHere(newEvent, itemList.size());
   }
   
   /**Remove the given event from the list.
    * @param whatToRemove Event to remove from the list.
    */
   public void removeEvent(Event whatToRemove) {
-    removeCalendarItem((CalendarItem) whatToRemove);
+    itemList.remove(whatToRemove);
   }
   
   /**
@@ -52,9 +79,28 @@ public class SortedEventList extends SortedCalendarItemList{
    * @return SortedEventList of the dates in the range.
    */
   public SortedEventList getEventsBetween(GregorianCalendar startRange, GregorianCalendar endRange) {
-	  ArrayList<CalendarItem> newItemList;
-	  newItemList = getDateRange(startRange, endRange);
-	  return new SortedEventList(newItemList);
+    ArrayList<Event> itemsInRange = new ArrayList<Event>(itemList.size());
+    for (int i = 0; i < itemList.size(); i++) {
+      if ( ( (this.getElementAt(i).getStartTime().after(startRange)) && 
+          (this.getElementAt(i).getStartTime().before(endRange)) )
+          || (this.getElementAt(i).getStartTime().equals(startRange)) || 
+              (this.getElementAt(i).getStartTime().equals(endRange)) ) {
+        itemsInRange.add(this.getElementAt(i));
+      }//end if
+    }//end for
+    return new SortedEventList(itemsInRange);
   }
   
+  /**
+   * @return the number of items in the list
+   */
+  public int getSize() {
+    return itemList.size();
+  }
+
+  @Override
+  public Event getElementAt(int index) {
+    Object[] itemArray = itemList.toArray();
+    return (Event) itemArray[index];
+  }
 }
