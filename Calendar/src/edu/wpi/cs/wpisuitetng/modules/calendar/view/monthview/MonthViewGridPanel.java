26a512433e6458331a4fd63f2f137c58b4062763
Veragoot <agaveyard@wpi.edu>
2013-12-10 20:47:25 -0500

    The REAL Iteration 4 Product
    Doesn't give weird times anymore

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/monthview/MonthViewGridPanel.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/monthview/MonthViewGridPanel.java
@@ -403,6 +403,46 @@ public class MonthViewGridPanel extends JPanel {
 		
 		GregorianCalendar commitmentStartTime = cmt.getStartTime();
 		int hour = commitmentStartTime.get(Calendar.HOUR_OF_DAY);
+		
+		if(hour > 12){ //if hour of day is past 12, change to proper time
+			if(hour == 13){
+				hour = 1;
+			}
+			if(hour == 14){
+				hour = 2;
+			}
+			if(hour == 15){
+				hour = 3;
+			}
+			if(hour == 16){
+				hour = 4;
+			}
+			if(hour == 17){
+				hour = 5;
+			}
+			if(hour == 18){
+				hour = 6;
+			}
+			if(hour == 19){
+				hour = 7;
+			}
+			if(hour == 20){
+				hour = 8;
+			}
+			if(hour == 21){
+				hour = 9;
+			}
+			if(hour == 22){
+				hour = 10;
+			}
+			if(hour == 23){
+				hour = 11;
+			}			
+		}
+		if(hour == 0){//if midnight change to 12
+			hour = 12;
+		}
+		
 		int minute = commitmentStartTime.get(Calendar.MINUTE);
 		
 		String AmOrPm = ""; //Set to either Am or Pm
