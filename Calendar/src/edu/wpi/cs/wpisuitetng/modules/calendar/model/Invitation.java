95246467cc3b01f15d52182e4c37b9d287faf446
Andrew Paon <ajpaon@wpi.edu>
2013-12-13 20:39:14 -0500

    reorganized Invitation.java, added HashMap for holding availability

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/Invitation.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/model/Invitation.java
@@ -10,7 +10,6 @@
 
 package edu.wpi.cs.wpisuitetng.modules.calendar.model;
 
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,262 +24,217 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 /**
  * The Class Invitation.
  * 
- * @author Eric
+ * @author Eric Wilcox, Andrew Paon
  */
 public class Invitation implements Model{
 
-  /** The name. */
-  private String name;
-    
-  /** The description. */
-  private String description;
-  
-  /** The id. */
-  private int id;
-  
-  /** The username. */
-  private String username;
-  
-  /** The status. */
-  private int status;
+	/** The name. */
+	private String name;
+
+	/** The date */
+	private String date;
+
+	/** The description. */
+	private String description;
+
+	/** The id. */
+	private int id;
+
+	/** HashMap containing availability at each time */
+	private HashMap<Integer, String[]> availablity;
+	
+	/** represents whether or not this goes to the entire team
+		currently removed until we add necessary functionality
+		
+	private boolean isTeamInvitation;
+	
+	*/
+	
+	/** Map of users to their permissions */
+	private Map<User, Permission> permissionMap = new HashMap<User, Permission>(); // annotation for User serialization
+
+	/** The project. */
+	private Project project;
+
+	/**
+	 * Instantiates a new invitation.
+	 *
+	 * @param name the name
+	 * @param startTime the start time
+	 * @param description the description
+	 */
+	public Invitation(String name, String date,
+			String description) {
+		this.name = name;
+		this.description = description;
+		this.id = -1;
+		this.date = date;
+		this.availablity = initializeAvailability();
+	}
+	
+	/**
+	 * creates a HashMap h that maps every hour of the workday
+	 * to an empty list of strings.  this string will eventually
+	 * contain the list of all available users for
+	 * each time 
+	 * 
+	 * @return h
+	 */
+	
+	private HashMap<Integer, String[]> initializeAvailability() {
+		HashMap<Integer, String[]> h = new HashMap<Integer, String[]>();
+		
+		for(int i=8; i<17; i++){
+			h.put(i, new String[0]);
+		}
+		
+		return h;
+	}
+
+	/** Getters and Setters */
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public HashMap<Integer, String[]> getAvailablity() {
+		return availablity;
+	}
+
+	public void setAvailablity(HashMap<Integer, String[]> availablity) {
+		this.availablity = availablity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	/**End of Getters and Setters.  Real Functions follow */
+	
+	
+	/**
+	 * Sets all the fields in the current Invitation equal to another's
+	 * 
+	 * @param inv the other invitation
+	 */
+	public void copy(Invitation inv){
+		this.setDescription(inv.getDescription());
+		this.setId(inv.getId());
+		this.setName(inv.getName());
+		this.setProject(inv.getProject());
+	}
+	
+	/**
+	 * From json.
+	 *
+	 * @param json the json
+	 * @return the invitation
+	 */
+	public static Invitation fromJSON(String json) {
+		final Gson parser = new Gson();
+		return parser.fromJson(json, Invitation.class);
+	}
+
+	/**
+	 * From json array.
+	 *
+	 * @param json the json
+	 * @return the invitation[]
+	 */
+	public static Invitation[] fromJsonArray(String json) {
+		final Gson parser = new Gson();
+		return parser.fromJson(json, Invitation[].class);
+	}
 
 
-  /** The is team invitation. */
-  private boolean isTeamInvitation = true;
+	/** Override functions */
+	
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#getPermission(edu.wpi.cs.wpisuitetng.modules.core.models.User)
+	 */
+	@Override
+	public Permission getPermission(User u) 
+	{
+		return permissionMap.get(u);
+	}
 
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#setPermission(edu.wpi.cs.wpisuitetng.Permission, edu.wpi.cs.wpisuitetng.modules.core.models.User)
+	 */
+	@Override
+	public void setPermission(Permission p, User u) 
+	{
+		permissionMap.put(u, p);
+	}
 
-  /**
-   * Checks if is team invitation.
-   *
-   * @return true, if is team invitation
-   */
-  public boolean isTeamInvitation() {
-    return isTeamInvitation;
-  }
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#getProject()
+	 */
+	@Override
+	public Project getProject() {
+		return project;
+	}
 
-  /**
-   * Sets the team invitation.
-   *
-   * @param isTeamInvitation the new team invitation
-   */
-  public void setTeamInvitation(boolean isTeamInvitation) {
-    this.isTeamInvitation = isTeamInvitation;
-  }
-  
-  /**
-   * Gets the status.
-   *
-   * @return the status
-   */
-  public int getStatus() {
-    return status;
-  }
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#setProject(edu.wpi.cs.wpisuitetng.modules.core.models.Project)
+	 */
+	@Override
+	public void setProject(Project p) {
+		this.project = p;
+	}
 
-  /**
-   * Sets the status.
-   *
-   * @param status the new status
-   */
-  public void setStatus(int status) {
-    this.status = status;
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#toJSON()
+	 */
+	@Override
+	public String toJSON() {
+		return new Gson().toJson(this, Invitation.class);
+	}
 
-  }
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#save()
+	 */
+	@Override
+	public void save() {
+	}
 
-  /** The permission map. */
-  private Map<User, Permission> permissionMap = new HashMap<User, Permission>(); // annotation for User serialization
-  
-  /** The project. */
-  private Project project;
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#delete()
+	 */
+	@Override
+	public void delete() {
+	}
 
-  /**
-   * Instantiates a new invitation.
-   *
-   * @param name the name
-   * @param startTime the start time
-   * @param description the description
-   */
-  public Invitation(String name, GregorianCalendar startTime,
-      String description) {
-    this.name = name;
-    this.description = description;
-    this.id = -1;
-    this.status = 1;
-  }
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#identify(java.lang.Object)
+	 */
 
-  /**
-   * Copy.
-   *
-   * @param comm the comm
-   */
-  public void copy(Invitation comm){
-    this.setDescription(comm.getDescription());
-    this.setId(comm.getId());
-    this.setName(comm.getName());
-    this.setProject(comm.getProject());
-    this.setStatus(comm.getStatus());
-  }
-
-  /**
-   * Gets the id.
-   *
-   * @return the id
-   */
-
-  public int getId() {
-    return id;
-  }
-
-  /**
-   * Sets the id.
-   *
-   * @param id the new id
-   */
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  /**
-   * Sets the name.
-   *
-   * @param name the new name
-   */
-  public void setName(String name){
-    this.name = name;
-  }
-
-  /**
-   * Sets the description.
-   *
-   * @param description the new description
-   */
-  public void setDescription(String description){
-    this.description = description;
-  }
-
-
-  
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#getPermission(edu.wpi.cs.wpisuitetng.modules.core.models.User)
-   */
-  @Override
-  public Permission getPermission(User u) 
-  {
-    return permissionMap.get(u);
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#setPermission(edu.wpi.cs.wpisuitetng.Permission, edu.wpi.cs.wpisuitetng.modules.core.models.User)
-   */
-  @Override
-  public void setPermission(Permission p, User u) 
-  {
-    permissionMap.put(u, p);
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#getProject()
-   */
-  @Override
-  public Project getProject() {
-    return project;
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#setProject(edu.wpi.cs.wpisuitetng.modules.core.models.Project)
-   */
-  @Override
-  public void setProject(Project p) {
-    this.project = p;
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#toJSON()
-   */
-  @Override
-  public String toJSON() {
-    return new Gson().toJson(this, Invitation.class);
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#save()
-   */
-  @Override
-  public void save() {
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#delete()
-   */
-  @Override
-  public void delete() {
-  }
-
-  /* (non-Javadoc)
-   * @see edu.wpi.cs.wpisuitetng.modules.Model#identify(java.lang.Object)
-   */
-  @Override
-  public Boolean identify(Object o) {
-    return null;
-  }
-
-  /**
-   * From json.
-   *
-   * @param json the json
-   * @return the invitation
-   */
-  public static Invitation fromJSON(String json) {
-    final Gson parser = new Gson();
-    return parser.fromJson(json, Invitation.class);
-  }
-
-  /**
-   * From json array.
-   *
-   * @param json the json
-   * @return the invitation[]
-   */
-  public static Invitation[] fromJsonArray(String json) {
-    final Gson parser = new Gson();
-    return parser.fromJson(json, Invitation[].class);
-  }
-
-
-  /**
-   * Gets the name.
-   *
-   * @return the name
-   */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Gets the description.
-   *
-   * @return the description
-   */
-  public String getDescription() {
-    return description;
-  }
-
-  /**
-   * Gets the username.
-   *
-   * @return the username
-   */
-  public String getUsername() {
-    return username;
-  }
-
-  /**
-   * Sets the username.
-   *
-   * @param username the new username
-   */
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-
+	@Override
+	public Boolean identify(Object o) {
+		return null;
+	}
 }
