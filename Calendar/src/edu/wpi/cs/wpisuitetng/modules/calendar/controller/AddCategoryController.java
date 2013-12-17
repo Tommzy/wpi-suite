1d726054dbad13bc463900c0a9cf80c27cfaff51
kramff <kramff@gmail.com>
2013-12-16 19:41:43 -0500

    Improved the documentation and cleaned up in AddCategoryController.java and CategoryRequestObserver.java

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddCategoryController.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/controller/AddCategoryController.java
@@ -10,13 +10,9 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
 
 import edu.wpi.cs.wpisuitetng.modules.calendar.categories.CategoriesModel;
-import edu.wpi.cs.wpisuitetng.modules.calendar.commitments.CommitmentsModel;
 import edu.wpi.cs.wpisuitetng.modules.calendar.model.Category;
-import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
 import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
@@ -31,30 +27,29 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
  */
 public class AddCategoryController implements ActionListener{
 
+	/** Reference to the CategoriesModel */
 	private final CategoriesModel model;
+	
+	/** The new category to add */
 	private final Category categoryToBeAdded;
-	 
-	//Category testCommit1 = new Category("First test",new GregorianCalendar(1992,8,19,23,4),"Success ><!");
-	 
+	
+	/** The observer associated with this controller */
+	AddCategoryRequestObserver observer = new AddCategoryRequestObserver(this);
+	
 	/**
-	 * Construct an AddCategoryController for the given model, view pair
+	 * Construct an AddCategoryController for the given model
 	 * @param model the model containing the messages
-	 * @param viewCategory the view where the user enters new messages
+	 * @param categoryToBeAdded the category to add
 	 */
 	public AddCategoryController(CategoriesModel model, Category categoryToBeAdded) {
 		this.model = model;
 		this.categoryToBeAdded = categoryToBeAdded;
 	}
-  
-	AddCategoryRequestObserver observer = new AddCategoryRequestObserver(this);
 	
-//	public void addTestToDatabase(){
-//		final Request request = Network.getInstance().makeRequest("calendar/category", HttpMethod.PUT); // PUT == create
-//		request.setBody(testCommit1.toJSON()); // put the new message in the body of the request
-//		request.addObserver(observer); // add an observer to process the response
-//		request.send();
-//	}
-	
+	/**
+	 * Make and send the request for adding the category to the server database.
+	 * @param category the category to add to the database
+	 */
 	public void addCategoryToDatabase(Category category){
 		final Request request = Network.getInstance().makeRequest("calendar/category", HttpMethod.PUT); // PUT == create
 		request.setBody(category.toJSON()); // put the new message in the body of the request
@@ -62,56 +57,27 @@ public class AddCategoryController implements ActionListener{
 		request.send();
 	}
 	
-	public Category testReturn(){
-		return observer.testReturn();
-	}
-	
-	 
-	/* 
+	/**
 	 * This method is called when the user clicks the Submit button
-	 * 
+	 * @param event The event for the action
 	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		
-		//addTestToDatabase();
-//		System.out.println("this is event!----->" + event.getActionCommand().toString());
-		// Get the text that was entered
-//		String name =  categoryToBeAdded.getName();
-//		GregorianCalendar startTime = categoryToBeAdded.getStartTime();
-//		String description = (String) categoryToBeAdded.getDescription();
-//		String invitee = (String) categoryToBeAdded.getInvitee();
-		 
-		// Make sure there is text
-		// OR THROUGH EXCEPTION?
-		
-//		//don't want to make a new event if there is no name
-//		if(name.length() > 0){
-//			Category sentCategory = new Category(name, startTime, description);
-//			// Add the message to the model
-//			model.addCategory(sentCategory);
-//		}
-		
 		// Send a request to the core to save this message
-		// Add the message to the model
 		final Request request = Network.getInstance().makeRequest("calendar/category", HttpMethod.PUT); // PUT == create
 		request.setBody(categoryToBeAdded.toJSON()); // put the new message in the body of the request
 		request.addObserver(new AddCategoryRequestObserver(this)); // add an observer to process the response
 		request.send(); // send the request
 		System.out.println("from AddCategoryController." + request.getBody());
-
-
 	}
 
 
-	/** ATTENTION AT THIS PART
+	/**
 	 * When the new Calendar item is received back from the server, add it to the local model.
-	 * @param message
+	 * @param item the category to add to the local model
 	 */
 	public void addCategoryToModel(Category item) {
 		model.addCategory(item);
 	}
 }
-
-
