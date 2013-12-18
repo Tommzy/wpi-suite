e3dc311a969ae52e0043ba38ae950200c14f675f
MastahShakes <cwturner@wpi.edu>
2013-12-17 22:16:29 -0500

    Just finished all my tests. Had to redo 20 of them 3 times because of git issues. I'm done.

---
+++ Calendar/test/ModelTest/CommitmentsModelTests.java
@@ -0,0 +1,125 @@
+package ModelTest;
+
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.Permission;
+import edu.wpi.cs.wpisuitetng.modules.calendar.commitments.CommitmentsModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Commitment;
+import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
+public class CommitmentsModelTests {
+
+	
+	
+	@Test
+	public void addCommitmentTest() {
+		//Set Up
+		
+		Commitment testCom1 = new Commitment("Test Commitment", null, "Test Commitment",null);
+		Commitment testCom2 = new Commitment("Copy Stuff", null ,"More CopyStuff",null);
+		
+		CommitmentsModel model = CommitmentsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addCommitment(testCom1);
+		model.addCommitment(testCom2);
+		
+		List<Commitment> comList = model.getAllCommitment();
+		
+		assertEquals(comList.size(), 2);
+		
+	}
+	
+	@Test
+	public void removeCommitmentTest() {
+		//Set Up
+		
+		Commitment testCom1 = new Commitment("Test Commitment", null, "Test Commitment",null);
+		testCom1.setId(1);
+		Commitment testCom2 = new Commitment("Copy Stuff", null ,"More CopyStuff",null);
+		testCom2.setId(2);
+		
+		CommitmentsModel model = CommitmentsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addCommitment(testCom1);
+		model.addCommitment(testCom2);
+		
+		model.removeCommitment(2);
+		
+		List<Commitment> comList = model.getAllCommitment();
+		
+		assertEquals(comList.size(), 1);
+		
+	}
+	
+	@Test
+	public void emptyModelTest() {
+		//Set Up
+		
+				Commitment testCom1 = new Commitment("Test Commitment", null, "Test Commitment",null);
+				Commitment testCom2 = new Commitment("Copy Stuff", null ,"More CopyStuff",null);
+				
+				CommitmentsModel model = CommitmentsModel.getInstance();
+
+				
+				//Tests
+				model.addCommitment(testCom1);
+				model.addCommitment(testCom2);
+				
+				List<Commitment> comList = model.getAllCommitment();
+				model.emptyModel();
+				assertEquals(comList.size(), 0);
+				
+	}
+	
+	
+	
+	@Test
+	public void getSizeTests() {
+		//Set Up
+		
+		Commitment testCom1 = new Commitment("Test Commitment", null, "Test Commitment",null);
+		Commitment testCom2 = new Commitment("Copy Stuff", null ,"More CopyStuff",null);
+		
+		CommitmentsModel model = CommitmentsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addCommitment(testCom1);
+		model.addCommitment(testCom2);
+		
+
+		assertEquals(model.getSize(), 2);
+		
+	}
+	
+	@Test
+	public void getCommitmentTest() {
+		//Set Up
+		
+		Commitment testCom1 = new Commitment("Test Commitment", null, "Test Commitment",null);
+		testCom1.setId(1);
+		Commitment testCom2 = new Commitment("Copy Stuff", null ,"More CopyStuff",null);
+		testCom2.setId(2);
+		
+		CommitmentsModel model = CommitmentsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addCommitment(testCom1);
+		model.addCommitment(testCom2);
+		
+		assertEquals(model.getCommitment(2), testCom2);
+		
+	}
+
+}
---
+++ Calendar/test/ModelTest/EventsModelTests.java
@@ -0,0 +1,125 @@
+package ModelTest;
+
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.Permission;
+import edu.wpi.cs.wpisuitetng.modules.calendar.events.EventsModel;
+import edu.wpi.cs.wpisuitetng.modules.calendar.model.Event;
+import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
+public class EventsModelTests {
+
+	
+	
+	@Test
+	public void addEventTest() {
+		//Set Up
+		
+		Event testCom1 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		Event testCom2 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		
+		EventsModel model = EventsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addEvent(testCom1);
+		model.addEvent(testCom2);
+		
+		List<Event> comList = model.getAllEvent();
+		
+		assertEquals(comList.size(), 2);
+		
+	}
+	
+	@Test
+	public void removeEventTest() {
+		//Set Up
+		
+		Event testCom1 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		testCom1.setId(1);
+		Event testCom2 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		testCom2.setId(2);
+		
+		EventsModel model = EventsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addEvent(testCom1);
+		model.addEvent(testCom2);
+		
+		model.removeEvent(2);
+		
+		List<Event> comList = model.getAllEvent();
+		
+		assertEquals(comList.size(), 1);
+		
+	}
+	
+	@Test
+	public void emptyModelTest() {
+		//Set Up
+		
+				Event testCom1 = new Event("Test Event", null, null, "no where", "Test Event",null);
+				Event testCom2 = new Event("Test Event", null, null, "no where", "Test Event",null);
+				
+				EventsModel model = EventsModel.getInstance();
+
+				
+				//Tests
+				model.addEvent(testCom1);
+				model.addEvent(testCom2);
+				
+				List<Event> comList = model.getAllEvent();
+				model.emptyModel();
+				assertEquals(comList.size(), 0);
+				
+	}
+	
+	
+	
+	@Test
+	public void getSizeTests() {
+		//Set Up
+		
+		Event testCom1 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		Event testCom2 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		
+		EventsModel model = EventsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addEvent(testCom1);
+		model.addEvent(testCom2);
+		
+
+		assertEquals(model.getSize(), 2);
+		
+	}
+	
+	@Test
+	public void getEventTest() {
+		//Set Up
+		
+		Event testCom1 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		testCom1.setId(1);
+		Event testCom2 = new Event("Test Event", null, null, "no where", "Test Event",null);
+		testCom2.setId(2);
+		
+		EventsModel model = EventsModel.getInstance();
+		model.emptyModel();
+		
+		//Tests
+		model.addEvent(testCom1);
+		model.addEvent(testCom2);
+		
+		assertEquals(model.getEvent(2), testCom2);
+		
+	}
+
+}
